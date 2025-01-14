dceab1e72d6d7a7b08f20831ccbf57e827985774
Ma2X <ma2xyz@protonmail.com>
2025-01-14 21:59:22 +0100

    Añadimos el texto de ayuda y borramos la (estupida) obligacion de tener el diccionario en el directorio diccionarios

--- Fuzzineitor.py
+++ Fuzzineitor.py
@@ -1,6 +1,18 @@
 import requests
 import sys
 
+ayuda = """
+[SINTAXIS]
+Fuzzineitor.py [opciones] [ruta del diccionario] [URL victima]
+
+Ejemplo: Fuzzineitor.py -s RockYou https://github.com
+
+[OPCIONES]
+-s: Indica que quieres hacer fuzzing de subdominio, el texto del diccionario se añadira con un punto (.) al principio del dominio
+
+-r: Indica que hacer fuzzing de ruta, el texto del diccionario se añadira con una barra (/) al final del dominio
+"""
+
 def subdominio(diccionario, dominio):
     for line in diccionario:
         direccion = dominio.replace("://", f"://{line.strip()}.")
@@ -19,12 +31,12 @@ def ruta(diccionario, dominio):
 if __name__ == "__main__":
 
     if sys.argv[1] == "-h":
-        print(help)
+        print(ayuda)
         sys.exit(0)
 
     if sys.argv[1] == "-s":
-        subdominio(open("diccionarios/" + sys.argv[2], "r"), sys.argv[3])
+        subdominio(open(sys.argv[2], "r"), sys.argv[3])
 
     if sys.argv[1] == "-r":
-        ruta(open("diccionarios/" + sys.argv[2], "r"), sys.argv[3])
+        ruta(open(sys.argv[2], "r"), sys.argv[3])
 sys.exit(0)
